8aa57de6f583a7c994eacc32c573b59102619732
Enzo Boulin <enzoboulin@gmail.com>
2026-01-22 11:01:18 +0100

    fix spinner

--- src/muffin/app.py
+++ src/muffin/app.py
@@ -1,22 +1,40 @@
 import streamlit as st
+from muffin.constant import LOGGING_LEVEL
 from muffin.main import main
+import random
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=LOGGING_LEVEL)
 
 
 BOT_NAME = "MC Muffin"
-SUBWAY_SURFERS_URL = "https://www.youtube.com/watch?v=QPW3XwBoQlw"
+WAINTING_URLS = [
+    "https://www.youtube.com/watch?v=QPW3XwBoQlw",
+    "https://www.youtube.com/watch?v=u7kdVe8q5zs",
+    "https://www.youtube.com/watch?v=nT4AtEvalSc",
+    "https://www.youtube.com/watch?v=TuGv1WIyUK4",
+]
+WAINTING_URL = random.choice(WAINTING_URLS)
 
 st.set_page_config(page_title=BOT_NAME, page_icon="🧁")
 
 st.title(f"🧁 {BOT_NAME}")
 st.subheader("Yo ! Dis-moi ce qu'il y a dans ton frigo !")
 
-
 user_prompt = st.text_input("Ingrédients (ex: chocolat, banane...)", "")
+
+placeholder = st.empty()
 
 if st.button("Trouver un muffin"):
     if user_prompt:
-        with st.spinner(f"{BOT_NAME} réfléchit..."):
-            st.video(SUBWAY_SURFERS_URL, loop=True, autoplay=True, muted=True)
-            st.markdown(main(user_prompt))
+        with placeholder.container():
+            with st.spinner(f"{BOT_NAME} réfléchit..."):
+                st.video(WAINTING_URL, loop=True, autoplay=True, muted=True)
+
+                result = main(user_prompt)
+
+        placeholder.empty()
+        st.markdown(result)
     else:
         st.warning("Veuillez entrer des ingrédients.")
